21141e407ad1c69033dab4a356bc28985c7cf619
fpwong <func.pwong@gmail.com>
2022-07-12 11:43:17 +1000

    Add CommonStateButton

---
+++ Source/FPCommon/Private/Widgets/FPCUWCommonStateButton.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Widgets/FPCUWCommonStateButton.h"
+
+void UFPCUWCommonStateButton::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	UpdateButtonTextFromState();
+}
+
+void UFPCUWCommonStateButton::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	UpdateButtonTextFromState();
+	OnClicked().AddUObject(this, &UFPCUWCommonStateButton::HandleToggleButtonClicked);
+}
+
+void UFPCUWCommonStateButton::HandleToggleButtonClicked()
+{
+	CurrentState = (CurrentState + 1) % States.Num();
+	UpdateButtonTextFromState();
+}
+
+void UFPCUWCommonStateButton::UpdateButtonTextFromState()
+{
+	if (!ButtonLabel)
+	{
+		return;
+	}
+
+	if (States.IsValidIndex(CurrentState))
+	{
+		ButtonLabel->SetText(States[CurrentState]);
+	}
+	else
+	{
+		ButtonLabel->SetText(FText::FromString("None"));
+	}
+}
---
+++ Source/FPCommon/Public/Widgets/FPCUWCommonStateButton.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FPCUWCommonButton.h"
+#include "FPCUWCommonStateButton.generated.h"
+
+UCLASS(Abstract, Blueprintable, ClassGroup = UI, meta = (Category = "FP Common UI", DisableNativeTick))
+class FPCOMMON_API UFPCUWCommonStateButton : public UFPCUWCommonButton
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	TArray<FText> States;
+
+	int32 CurrentState = 0;
+
+	virtual void NativePreConstruct() override;
+
+	virtual void NativeOnInitialized() override;
+
+	UFUNCTION()
+	virtual void HandleToggleButtonClicked();
+
+	virtual void UpdateButtonTextFromState();
+};
