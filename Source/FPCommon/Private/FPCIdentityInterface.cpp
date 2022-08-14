d397d49995ab929e4179e183b929dbb152c28eed
fpwong <func.pwong@gmail.com>
2022-08-14 17:51:17 +1000

    Add identity interface

---
+++ Source/FPCommon/Private/FPCIdentityInterface.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPCIdentityInterface.h"
+
+FGameplayTag UFPCIdentityHelper::GetIdentityTag(UObject* Object)
+{
+	if (Object->Implements<UFPCIdentityInterface>())
+	{
+		return IFPCIdentityInterface::Execute_GetIdentityTag(Object);
+	}
+
+	return FGameplayTag();
+}
---
+++ Source/FPCommon/Public/FPCIdentityInterface.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Interface.h"
+#include "FPCIdentityInterface.generated.h"
+
+UCLASS(config=Game)
+class FPCOMMON_API UFPCIdentityHelper : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static FGameplayTag GetIdentityTag(UObject* Object);
+};
+
+// This class does not need to be modified.
+UINTERFACE()
+class UFPCIdentityInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class FPCOMMON_API IFPCIdentityInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category="FPCommon")
+	FGameplayTag GetIdentityTag();
+};
