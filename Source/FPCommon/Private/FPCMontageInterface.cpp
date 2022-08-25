b549b213d810a6e76cbc7d81646726db22a10d3d
fpwong <func.pwong@gmail.com>
2022-08-25 22:04:21 +1000

    Add MontageInterface

---
+++ Source/FPCommon/Private/FPCMontageInterface.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPCMontageInterface.h"
+
+// Add default functionality here for any IFPCMontageInterface functions that are not pure virtual.
---
+++ Source/FPCommon/Public/FPCMontageInterface.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Interface.h"
+#include "FPCMontageInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UFPCMontageInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class FPCOMMON_API IFPCMontageInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	UAnimMontage* GetMontageByTag(FGameplayTag GameplayTag);
+};
