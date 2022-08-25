1b783c1f7fedb0b7b116d9964175cf3fbc7c5e99
fpwong <func.pwong@gmail.com>
2022-08-25 11:31:21 +1000

    Add AsyncTask TimedEvents

---
+++ Source/FPCommon/Private/FPCAsyncTask_TimedEvents.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPCAsyncTask_TimedEvents.h"
+
+UFPCAsyncTask_TimedEvents* UFPCAsyncTask_TimedEvents::CreatedTimedEvents(UObject* WorldContextObject, TArray<FTimedEvent> Events, bool bStartImmediately)
+{
+	if (Events.Num() == 0)
+	{
+		return nullptr;
+	}
+
+	UFPCAsyncTask_TimedEvents* TimedEvents = NewObject<UFPCAsyncTask_TimedEvents>(WorldContextObject);
+	TimedEvents->Events = Events;
+
+	if (bStartImmediately)
+	{
+		TimedEvents->StartTimedEvent();
+	}
+
+	return TimedEvents;
+}
+
+void UFPCAsyncTask_TimedEvents::OnTick()
+{
+	const float TimeSinceStart = GetWorld()->GetTimeSeconds() - StartTime;
+
+	// assume events are in time order
+	for (int i = NextEventIndex; i < Events.Num(); ++i)
+	{
+		if (TimeSinceStart <= Events[i].Time)
+		{
+			break;
+		}
+
+		// trigger the event since our time is greater than the event
+		NextEventIndex = i + 1;
+
+		OnTimedEvent.Broadcast(Events[i].EventTag);
+
+		UE_LOG(LogTemp, Warning, TEXT("%d"), i);
+
+		// we have reached the end, end the task
+		if (i == Events.Num())
+		{
+			EndTask();
+		}
+	}
+}
+
+TStatId UFPCAsyncTask_TimedEvents::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(UFPCAsyncTask_TimedEvents, STATGROUP_Tickables);
+}
+
+void UFPCAsyncTask_TimedEvents::Tick(float DeltaTime)
+{
+	if (StartTime >= 0)
+	{
+		OnTick();
+	}
+}
+
+void UFPCAsyncTask_TimedEvents::StartTimedEvent()
+{
+	if (!TickHandle.IsValid())
+	{
+		StartTime = GetWorld()->GetTimeSeconds();
+		// GetWorld()->GetTimerManager().SetTimer(TickHandle, this, &UFPCAsyncTask_TimedEvents::OnTick, 0.0f, true);
+	}
+}
+
+void UFPCAsyncTask_TimedEvents::EndTask()
+{
+	SetReadyToDestroy();
+	MarkAsGarbage();
+
+	if (TickHandle.IsValid())
+	{
+		GetWorld()->GetTimerManager().ClearTimer(TickHandle);
+		TickHandle.Invalidate();
+	}
+}
---
+++ Source/FPCommon/Public/FPCAsyncTask_TimedEvents.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "FPCAsyncTask_TimedEvents.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTimedEvent, FGameplayTag, EventTag);
+
+USTRUCT(BlueprintType)
+struct FTimedEvent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Time;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FGameplayTag EventTag;
+};
+
+UCLASS(BlueprintType, meta=(ExposedAsyncProxy = AsyncTask))
+class FPCOMMON_API UFPCAsyncTask_TimedEvents
+	: public UBlueprintAsyncActionBase
+	, public FTickableGameObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnTimedEvent OnTimedEvent;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"))
+	static UFPCAsyncTask_TimedEvents* CreatedTimedEvents(UObject* WorldContextObject, TArray<FTimedEvent> Events, bool bStartImmediately = true);
+
+	UFUNCTION(BlueprintCallable)
+	void StartTimedEvent();
+
+protected:
+	// ~ FTickableGameObject
+	void OnTick();
+	virtual TStatId GetStatId() const override;
+	// ~ FTickableGameObject
+
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void EndTask();
+public:
+private:
+	TArray<FTimedEvent> Events;
+	FTimerHandle TickHandle;
+	float StartTime = -1;
+
+	int NextEventIndex = 0;
+};
