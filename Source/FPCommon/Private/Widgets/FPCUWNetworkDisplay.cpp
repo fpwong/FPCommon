ee411f94bc33a84008810ebfb0b6fafea959a3da
fpwong <func.pwong@gmail.com>
2023-02-02 14:23:04 +1100

    Add user widget to display current network role

---
+++ Source/FPCommon/Private/Widgets/FPCUWNetworkDisplay.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Widgets/FPCUWNetworkDisplay.h"
+
+#include "CommonTextBlock.h"
+
+void UFPCUWNetworkDisplay::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (NetworkLabel)
+	{
+		const bool bIsServer = GetWorld()->GetFirstPlayerController()->HasAuthority();
+		const FText Text = bIsServer ? INVTEXT("Server") : INVTEXT("Client");
+		NetworkLabel->SetText(Text);
+	}
+}
---
+++ Source/FPCommon/Public/Widgets/FPCUWNetworkDisplay.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTextBlock.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/Object.h"
+#include "FPCUWNetworkDisplay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPCOMMON_API UFPCUWNetworkDisplay : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UCommonTextBlock* NetworkLabel;
+
+	virtual void NativeOnInitialized() override;
+};
