a889e2f701dbe627e63629e95bb9107e54f3b035
fpwong <func.pwong@gmail.com>
2024-01-17 19:25:34 +1100

    Add FPMaterialButton

---
+++ Source/FPCommon/Private/Widgets/FPMaterialButton.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Widgets/FPMaterialButton.h"
+
+#include "CommonTextBlock.h"
+#include "Components/Image.h"
+#include "Kismet/KismetMaterialLibrary.h"
+
+void UFPMaterialButton::NativeOnCurrentTextStyleChanged()
+{
+	Super::NativeOnCurrentTextStyleChanged();
+
+	if (ButtonLabel)
+	{
+		ButtonLabel->SetStyle(GetCurrentTextStyleClass());
+	}
+}
+
+bool UFPMaterialButton::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	if (ButtonLabel)
+	{
+		ButtonLabel->SetText(InitialButtonText);
+	}
+
+	if (ButtonMaterial)
+	{
+		DynamicMaterial = UKismetMaterialLibrary::CreateDynamicMaterialInstance(this, ButtonMaterial);
+		ButtonImage->SetBrushFromMaterial(DynamicMaterial);
+	}
+
+	return true;
+}
+
+void UFPMaterialButton::SetButtonLabelText(const FText& InText)
+{
+	if (ButtonLabel)
+	{
+		ButtonLabel->SetText(InText);
+	}
+}
+
+void UFPMaterialButton::NativeOnHovered()
+{
+	Super::NativeOnHovered();
+
+	if (DynamicMaterial)
+	{
+		DynamicMaterial->SetScalarParameterValue("IsHovered", true);
+	}
+}
+
+void UFPMaterialButton::NativeOnUnhovered()
+{
+	Super::NativeOnUnhovered();
+
+	if (DynamicMaterial)
+	{
+		DynamicMaterial->SetScalarParameterValue("IsHovered", false);
+	}
+}
+
+#if WITH_EDITOR
+const FText UFPMaterialButton::GetPaletteCategory()
+{
+	return FText::FromString("FPCommon");
+}
+#endif
---
+++ Source/FPCommon/Public/Widgets/FPMaterialButton.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "CommonTextBlock.h"
+#include "FPMaterialButton.generated.h"
+
+class UImage;
+
+UCLASS(Abstract, Blueprintable, ClassGroup = UI, meta = (Category = "FP Common UI", DisableNativeTick))
+class FPCOMMON_API UFPMaterialButton : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeOnCurrentTextStyleChanged() override;
+
+	virtual bool Initialize() override;
+
+	UPROPERTY(EditAnywhere)
+	UMaterialInterface* ButtonMaterial;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UImage* ButtonImage;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	UCommonTextBlock* ButtonLabel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "FPCommonButton", meta = (ExposeOnSpawn = true, EditCondition = "ButtonLabel != nullptr", EditConditionHides))
+	FText InitialButtonText;
+
+	void SetButtonLabelText(const FText& InText);
+
+	UPROPERTY(BlueprintReadOnly)
+	UMaterialInstanceDynamic* DynamicMaterial;
+
+	virtual void NativeOnHovered() override;
+	virtual void NativeOnUnhovered() override;
+
+#if WITH_EDITOR
+	virtual const FText GetPaletteCategory() override;
+#endif
+};
