5d4a1b341bbeaec1f638d657acf3c515e9a426f3
fpwong <func.pwong@gmail.com>
2024-11-19 16:40:10 +1100

    Add imgui helper class

---
+++ Source/FPCommon/Public/FPLog.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "Logging/StructuredLog.h"
+
+// class FPCOMMON_API FFPLog
+// {
+// public:
+// 	static FString GetWorldNetMode(UWorld* World)
+// 	{
+// 		if (World && World->WorldType == EWorldType::PIE)
+// 		{
+// 			switch (World->GetNetMode())
+// 			{
+// 			case NM_Client:
+// 				return FString::Printf(TEXT("Client %d: "), static_cast<int>(GPlayInEditorID));
+// 			case NM_DedicatedServer:
+// 			case NM_ListenServer:
+// 				return FString::Printf(TEXT("Server: "));
+// 			case NM_Standalone:
+// 				break;
+// 			}
+// 		}
+// 		return "";
+// 	}
+// };
+
+#define FP_STRFMT(Fmt, ...) FString::Format(TEXT(Fmt), { __VA_ARGS__ })
+#define FP_IMGUILOG(Fmt, ...) IMGUI_DEBUG_LOG(TCHAR_TO_ANSI(*FP_STRFMT(Fmt, __VA_ARGS__)))
+#define FP_IMGUITEXT(Fmt, ...) ImGui::Text(TCHAR_TO_ANSI(*FP_STRFMT(Fmt, __VA_ARGS__)))
+#define FP_IMGUILOG_WRAPPED(Fmt, ...) const ImGui::FScopedContext ScopedContext; { if (ScopedContext) FP_IMGUILOG(Fmt, { __VA_ARGS__ }); }
+// #define FP_LOGWORLD(World, CategoryName, Verbosity, Format, ...) UE_LOGFMT(CategoryName, Verbosity, TEXT(FFPLog::GetWorldNetMode(World) + Format), __VA_ARGS__)
