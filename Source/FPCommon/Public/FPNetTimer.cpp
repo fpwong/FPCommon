2c6d2f1eae6f93f4e827728049dadcf5ab2fcefe
fpwong <func.pwong@gmail.com>
2025-01-17 18:47:19 +1100

    Add net timer

---
+++ Source/FPCommon/Public/FPNetTimer.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPNetTimer.h"
+
+#include "GameFramework/GameState.h"
+
+void FFPNetTimer::StartTimer(UObject* WorldContext)
+{
+	if (WorldContext)
+	{
+		StartTime = WorldContext->GetWorld()->GetGameStateChecked<AGameState>()->GetServerWorldTimeSeconds();
+	}
+}
+
+float FFPNetTimer::GetTimeRemaining(UObject* WorldContext) const
+{
+	if (!WorldContext)
+	{
+		return 0;
+	}
+
+	const double CurrTime = WorldContext->GetWorld()->GetGameStateChecked<AGameState>()->GetServerWorldTimeSeconds();
+	return Duration - (CurrTime - StartTime);
+}
---
+++ Source/FPCommon/Public/FPNetTimer.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "FPNetTimer.generated.h"
+
+USTRUCT()
+struct FPCOMMON_API FFPNetTimer
+{
+	GENERATED_BODY()
+
+	void StartTimer(UObject* WorldContext);
+
+	float GetTimeRemaining(UObject* WorldContext) const;
+
+	bool HasEnded(UObject* WorldContext) const { return IsValid() && GetTimeRemaining(WorldContext) <= 0.0f; }
+
+	bool IsValid() const { return StartTime >= 0; }
+
+	void Clear() { StartTime = INDEX_NONE; };
+
+	UPROPERTY(EditAnywhere)
+	float Duration = 5.0f;
+
+	UPROPERTY(Transient)
+	double StartTime = INDEX_NONE;
+};
