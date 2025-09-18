3031aa68547067664435f11ed252d4ee180bd071
fpwong <func.pwong@gmail.com>
2025-09-18 16:08:34 +1000

    Disable montage interface

--- Source/FPCommon/Private/FPCMontageInterface.cpp
+++ Source/FPCommon/Private/FPCMontageInterface.cpp
@@ -1,5 +1,5 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "FPCMontageInterface.h"
-
-// Add default functionality here for any IFPCMontageInterface functions that are not pure virtual.
+﻿// // Fill out your copyright notice in the Description page of Project Settings.
+//
+// #include "FPCMontageInterface.h"
+//
+// // Add default functionality here for any IFPCMontageInterface functions that are not pure virtual.
--- Source/FPCommon/Public/FPCMontageInterface.h
+++ Source/FPCommon/Public/FPCMontageInterface.h
@@ -1,27 +1,27 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "GameplayTagContainer.h"
-#include "UObject/Interface.h"
-#include "FPCMontageInterface.generated.h"
-
-// This class does not need to be modified.
-UINTERFACE()
-class UFPCMontageInterface : public UInterface
-{
-	GENERATED_BODY()
-};
-
-/**
- * 
- */
-class FPCOMMON_API IFPCMontageInterface
-{
-	GENERATED_BODY()
-
-public:
-	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
-	UAnimMontage* GetMontageByTag(FGameplayTag GameplayTag);
-};
+﻿// // Fill out your copyright notice in the Description page of Project Settings.
+//
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "GameplayTagContainer.h"
+// #include "UObject/Interface.h"
+// #include "FPCMontageInterface.generated.h"
+//
+// // This class does not need to be modified.
+// UINTERFACE()
+// class UFPCMontageInterface : public UInterface
+// {
+// 	GENERATED_BODY()
+// };
+//
+// /**
+//  * 
+//  */
+// class FPCOMMON_API IFPCMontageInterface
+// {
+// 	GENERATED_BODY()
+//
+// public:
+// 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+// 	UAnimMontage* GetMontageByTag(FGameplayTag GameplayTag);
+// };
